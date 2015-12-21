d385862be2f317c55e8853b43ab405101ee08e99
Jeremy Faller <jfaller@google.com>
2015-12-21 15:20:17 -0500

    continued cleanup for release

--- google3/third_party/py/earthquakes/appengine/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-blaze build  third_party/py/earthquakes/appengine:earthquakes
-/google/data/ro/buildstatic/projects/apphosting/tools/appcfg_over_stubby.par \
-  update blaze-bin/third_party/py/earthquakes/appengine/earthquakes.runfiles
